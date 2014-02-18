36707d71be5fad53d70e2ec09aa37b154276fb63
Stewart Allen <stewart@neuron.com>
2014-02-18 13:54:36 -0500

    add db dump/load

--- start.js
+++ start.js
@@ -1,3 +1,17 @@
 var boot = require('hydra-boot');
 
-boot.init();
+var arg = process.argv.slice();
+var nvm = arg.shift();
+var app = arg.shift();
+
+switch (arg.length > 0 ? arg[0] : 'init') {
+    case 'init':
+        boot.init();
+        break;
+    case 'dump':
+        boot.dump(arg[1]);
+        break;
+    case 'load':
+        boot.load(arg[1]);
+        break;
+}

0a2d3d6282e72936bc3690f87eb0f872f1d43b0f
Stewart Allen <stewart@neuron.com>
2014-02-19 16:52:37 -0500

    add local test mode

--- start.js
+++ start.js
@@ -4,14 +4,21 @@ var arg = process.argv.slice();
 var nvm = arg.shift();
 var app = arg.shift();
 
-switch (arg.length > 0 ? arg[0] : 'init') {
-    case 'init':
-        boot.init();
-        break;
-    case 'dump':
-        boot.dump(arg[1]);
-        break;
-    case 'load':
-        boot.load(arg[1]);
-        break;
+if (process.argv.length >= 2) {
+    switch (arg.length > 0 ? arg[0] : null) {
+        case 'init':
+            boot.init();
+            break;
+        case 'dump':
+            boot.dump(arg[1]);
+            break;
+        case 'load':
+            boot.load(arg[1]);
+            break;
+        default:
+            console.log("start [ init | dump | load]");
+            break;
+    }
 }
+
+module.exports = boot;
